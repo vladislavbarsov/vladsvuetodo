76baba73c1bfbfd3fe7ec611744d6f23546791c7
vladislavbarsov <35625801+vladislavbarsov@users.noreply.github.com>
2020-05-11 18:37:44 +0300

    added Vuex
    i am adding Vuex so that i can make some of the local variables global for the whole project. Namely i will make the boolean variable "editMode" global, since it will help solve some bugs.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import LongPress from 'vue-directive-long-press'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ Vue.directive('long-press', LongPress);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
